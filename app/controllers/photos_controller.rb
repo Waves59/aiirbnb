7d40ddd2cafe95b7868c4673907e2e3534f0a1bf
Thibault Demars <thibault.demars59@gmail.com>
2018-02-08 00:18:20 +0000

    Patch suppression des images

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,20 @@
 class PhotosController < ApplicationController
     
-    def destroy
-        @photo = Photo.find(params[:id])
-        room = @photo.room
-        
-        @photo.destroy
-        
-        @photos = Photo.where(room_id: room.id)
-        
-        respond_to :js
-    end
+     def destroy
+    
+          @photo = Photo.find(params[:id])
+    
+           room = @photo.room
+    
+      
+          @photo.destroy
+    
+      
+          @photos = Photo.where(room_id: room.id)
+    
+      
+           respond_to :js
+     end
+
     
 end
